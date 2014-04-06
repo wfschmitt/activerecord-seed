7ebeaa073a431210df02430f9378aded938dd219
wschmitt <wfschmittx@gmail.com>
2014-04-06 03:20:31 +0200

    new version

--- lib/active_record/seed/version.rb
+++ lib/active_record/seed/version.rb
@@ -1,6 +1,5 @@
 module ActiveRecord
   module Seed
-    puts File.expand_path('../../../VERSION', __FILE__)
     VERSION = File.read(File.expand_path('../../../../VERSION', __FILE__)).chomp
   end
 end
